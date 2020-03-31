491dfeaa76bc4f2613b413c49aa791f489286b76
liushuai@ideal-link.com <shuai515>
2020-03-31 18:40:34 +0800

    org

--- src/main/java/com/jay/springcloundorg/rest/TestController.java
+++ src/main/java/com/jay/springcloundorg/rest/TestController.java
@@ -2,6 +2,7 @@ package com.jay.springcloundorg.rest;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/3/29 14:01
  */
 @RestController
+//config刷新 会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+//#POST 请求的方式来访问服务端 http://localhost:port/actuator/refresh 就会更新配置文件至最新版本   这是针对单个客户端的刷新机制
+@RefreshScope
 @RequestMapping("/webApi/test")
 public class TestController {
     @Value("${eureka.client.registry-fetch-interval-seconds}")
